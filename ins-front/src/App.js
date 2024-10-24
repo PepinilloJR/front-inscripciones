e7613d2c489a560fd36e88628a5a0b314fd001f9
Francisco Monasterolo <monasterolofrancisco@gmail.com>
2024-10-24 17:52:53 -0300

    detalles

--- ins-front/src/App.js
+++ ins-front/src/App.js
@@ -2,25 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from './Pages/home/home';
-
 import { GeneralContext } from './Context/Context';
-import { useContext } from 'react';
 
 function App() {
 
-  const Gcontext = useContext(GeneralContext)
-
-
-
   return (
     <div className="App">
-      <Gcontext.Provider value={null}>
+      <GeneralContext.Provider value={null}>
         <BrowserRouter>
           <Routes>
-            <Route path='\' element={<Home />} />
+            <Route path='/' element={<Home />} />
           </Routes>
         </BrowserRouter>
-      </Gcontext.Provider>
+      </GeneralContext.Provider>
     </div>
   );
 }
