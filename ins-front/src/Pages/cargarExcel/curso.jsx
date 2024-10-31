3e945ecaa7721b1361dbd5e8a3073691fea7283b
Francisco Monasterolo <monasterolofrancisco@gmail.com>
2024-10-31 15:52:22 -0300

    prueba prototipo

--- ins-front/src/Pages/cargarExcel/curso.jsx
+++ ins-front/src/Pages/cargarExcel/curso.jsx
@@ -14,9 +14,8 @@ function Curso({value}) {
             </div>
             <div>
                 {seleccion ? value[1].map((materia, key_) => {
-                    return <div key={key_} className="Materia">
-                        {materia.materia}
-                    </div>
+
+                    return <Materia key={key_} materia={materia}></Materia>
                 }
                 ) : <div></div>
                 }
@@ -27,5 +26,31 @@ function Curso({value}) {
 
 }
 
+//<div key={key_} className="Materia">
+//                        {materia.materia}
+//                        <div className="">
+//                            {tieneAlumnos ? materia.alumnos[0].Nombre : "sin inscripciones" }
+//                       </div>
+//                    </div>
+
+function Materia({materia}) {
+
+    const [seleccion, setSeleccion] = useState(false)
+
+    return <><div onClick={()=> {setSeleccion(!seleccion)}} className={seleccion ? "MateriaSeleccionada" : "Materia"}>
+        {materia.materia}
+        </div>
+        <div>
+            {seleccion ?
+            <div className="Inscripciones">
+                {materia.alumnos !== undefined ? materia.alumnos.map((alumno, key) => {
+                    return <div className="Alumno">
+                        <div>{alumno.Nombre}</div>
+                    </div>
+                }) : "sin inscripciones" }
+            </div> : <div></div>}
+        </div>
+        </>
+}
 
 export default Curso
--- ins-front/src/Pages/cargarExcel/listaCupos.jsx
+++ ins-front/src/Pages/cargarExcel/listaCupos.jsx
@@ -9,12 +9,12 @@ function ListadoCupos() {
     const {listaCupos, listaIns} = useContext(ListadoContext)
 
     const [Cursos, setCursos] = useState([])
-
+    const [Alumnos, setAlumnos] = useState(new Map())
 
     // funcion temporal hasta tener esto en el back, filtro los cursos para luego listar a partir de cada uno las materias, cupos, etc.
     // es decir, estoy armando un formato para listar todo
     // la idea seria
-    // tabla de cursos = [... curso -> { materias: [{nombre: asda, cupos: 23, ...} ...]  }
+    // tabla de cursos = [... curso -> { materias: [{nombre: asda, cupos: 23, inscriptos: [...] ...} ...]  }
     
     // como digo, eso deberia venir desde el back ya armadito, si no tendria que hacer esto todo el rato
     const filtrarCursos = () => {
@@ -25,17 +25,41 @@ function ListadoCupos() {
         
             var listaMaterias = tabla.get(o.curso)
             if (listaMaterias) {
-                listaMaterias = listaMaterias.push({materia: o.materia, cupo: o.cupo, inscriptos: o.inscriptos})
+                //console.log(Alumnos.get(o.curso+o.materia))
+                listaMaterias = listaMaterias.push({materia: o.materia, cupo: o.cupo, inscriptos: o.inscriptos, alumnos: Alumnos.get(o.curso+o.materia)})
                 //console.log(tabla)
             } else {
-                tabla.set(o.curso, [{materia: o.materia, cupo: o.cupo, inscriptos: o.inscriptos}])
+                tabla.set(o.curso, [{materia: o.materia, cupo: o.cupo, inscriptos: o.inscriptos, alumnos: Alumnos.get(o.curso+o.materia)}])
             }
         }
         
-
+        console.log(tabla)
         setCursos(tabla)
 
     }
+
+    // creo un mapa para poder relacionar rapidamente los alumnos con el otro mapa de cursos materias etc
+    const filtrarInscriptos = () => {
+        const tabla = new Map();
+        
+        for (const o of listaIns) {
+        
+            var listaInscripciones = tabla.get(o["Comisión (Opción 1)"]+o["Materia "])
+            if (listaInscripciones) {
+
+                listaInscripciones = listaInscripciones.push(o)
+                //console.log(tabla)
+            } else {
+                tabla.set(o["Comisión (Opción 1)"]+o["Materia "], [o])
+            }
+        }
+        
+        console.log(tabla)
+        setAlumnos(tabla)
+
+
+    }
+
 
     useEffect(()=> {
         //console.log(Cursos)
@@ -46,12 +70,18 @@ function ListadoCupos() {
             filtrarCursos()
         }
 
-    }, [listaCupos])
-
+    }, [listaCupos, Alumnos])
 
     useEffect(() => {
-        console.log(listaCupos)
-        console.log(listaIns)
+        if (listaIns) {
+            filtrarInscriptos()
+            //console.log(Alumnos)
+        }
+    }, [listaIns])
+
+    useEffect(() => {
+        //console.log(listaCupos)
+        //console.log(listaIns)
     }, [listaIns, listaCupos])
 
 
