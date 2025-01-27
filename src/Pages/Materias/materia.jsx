2395de926169449b58d830abf2a99f9ab7dbaaae
Francisco Monasterolo <pepinillosverdes86@gmail.com>
2025-01-27 19:43:02 -0300

    quitando ese link malvado

--- src/Pages/Materias/materia.jsx
+++ src/Pages/Materias/materia.jsx
@@ -7,13 +7,11 @@ function Materia({ mat }) {
     const { setMateriaSelected, setModal } = useContext(GeneralContext)
 
     return <>
-    <Link to="/inscripciones">
         <div onClick={() => { setMateriaSelected(mat); /*setModal(<MateriaModal></MateriaModal>)*/ }} className="Materia">
             <div className="MateriaTitulo">
                 {mat.nombre}
             </div>
         </div>
-    </Link>
     </>
 
 }
